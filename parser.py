2c5478fcf4edacc624f3382d12f418cf3bb248cc
vikram-kv <vikram.kv2001@gmail.com>
2022-10-19 13:49:27 +0530

    added grammar rules for yacc in parser.py

--- parser.py
+++ parser.py
@@ -1,9 +1,64 @@
 # combined lexical analyzer and parser
 
+from re import I
 from ply.lex import lex
 from ply.yacc import yacc
+from helpers import *
 
 # tokens identified by the lexer
 tokens = ('space', 'fullvowel', 'kaki', 'conjsyll2', 'conjsyll1', 'nukchan', 'yarule', 'consonant', 'vowel', 'halant', 'matra')
 
 
+
+
+# parser part
+
+def p_sentence(p):
+    '''
+    sentence : words
+    '''
+    global flags, words
+    if flags.parseLevel == 0:
+        words.syllabifiedWordOut = p[1]
+
+        if words.syllabifiedWordOut.find('&&') != -1:
+            words.syllabifiedWordOut = words.syllabifiedWordOut.replace("&&","&")
+        
+        flags.parseLevel += 1
+    else:
+        words.phonifiedWord = p[1]
+
+
+def p_words_syltoken(p):
+    '''
+    words : syltoken
+    '''
+    global flags
+    if flags.DEBUG:
+        print(f'\nSyll:\t{p[1]}')
+    p[0] = p[1]
+
+def p_words_wordsandsyltoken(p):
+    '''
+    words : words syltoken
+    '''
+    p[0] = p[1] + p[2]
+
+def p_syltoken(p):
+    '''
+    syltoken : fullvowel
+             | kaki
+             | conjsyll2
+             | conjsyll1 
+             | nukchan
+             | yarule
+             | consonant
+             | vowel
+             | halant
+             | matra
+    '''
+    p[0] = p[1]
+
+if __name__ == '__main__':
+    parser = yacc()
+    pass
