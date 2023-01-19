897eed300322018594cfab590d375ee27b729967
vikram-kv <vikram.kv2001@gmail.com>
2023-01-19 16:15:19 +0530

    .

---
+++ extract_words.py
@@ -0,0 +1,33 @@
+import os, shutil
+from parallelparser import wordparse
+from joblib import Parallel, delayed
+from tqdm import tqdm
+
+num_jobs = 20
+infolder = 'Original'
+outfolder = 'Words'
+
+for fdr in [outfolder]:
+    if os.path.exists(fdr):
+        shutil.rmtree(fdr)
+    os.mkdir(fdr)
+
+flist = os.listdir(infolder)
+for fname in flist:
+    with open(f'{infolder}/{fname}', 'r') as f:
+        cnts = f.readlines()
+
+    i = 0
+    
+    words = []
+    for l in cnts:
+        l = l.strip().split('\t')
+        words.append(l[0])
+    
+    fout = fname.split('_')[1]
+    fout = fout.split('.')[0]
+    print(fout)
+
+    with open(f'{outfolder}/{fout}.words', 'w') as f:
+        for w in words:
+            f.write(w + '\n')
---
+++ run_parser_all_lang_all_opt.py
@@ -0,0 +1,49 @@
+import os, shutil
+from parallelparser import wordparse
+from joblib import Parallel, delayed
+from tqdm import tqdm
+
+num_jobs = 20
+infolder = 'Words'
+outfolder = 'Outputs'
+
+for fdr in [outfolder]:
+    if os.path.exists(fdr):
+        shutil.rmtree(fdr)
+    os.mkdir(fdr)
+
+langs = []
+langwdmap = {}
+
+flist = os.listdir(infolder)
+for fname in flist:
+    with open(f'{infolder}/{fname}', 'r') as f:
+        cnts = f.readlines()
+
+    i = 0
+    
+    words = []
+    for l in cnts:
+        l = l.strip().split('\t')
+        words.append(l[0])
+    
+    lan = fname.split('.')[0]
+    langs.append(lan)
+    langwdmap[lan] = words
+
+for opt in [0,1,2]:
+    out_cur = outfolder + '/option-' + str(opt)
+    os.mkdir(out_cur)
+
+    for l in langs:
+        words = langwdmap[l]
+
+        anslist = Parallel(n_jobs=num_jobs)(delayed(wordparse)(wd, 1, opt) for wd in tqdm(words))
+
+        with open(f'{out_cur}/{l}.output', 'w') as f:
+            for i in range(len(words)):
+                f.write(f'{words[i]}\t{anslist[i]}\n')
+        
+        print(f'completed {l}')
+
+    
